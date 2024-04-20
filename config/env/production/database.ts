97c0ef9c4be328075c0a0e008f918f3fe155b407
shivansh02 <ss398@snu.edu.in>
2024-04-20 16:49:04 +0530

    supabase db connected

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -1,17 +1,15 @@
 module.exports = ({ env }) => ({
+
+  connection: {
+    client: 'postgres',
     connection: {
-      client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        schema: env('DATABASE_SCHEMA', 'public'), // Not required
-        ssl: {
-          ca: env('DATABASE_CA'),
-        },
-      },
-      debug: false,
+      host: env('DATABASE_HOST', 'aws-0-ap-south-1.pooler.supabase.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'postgres'),
+      user: env('DATABASE_USERNAME', 'postgres.dsbstoloyppvfghqyxtm'),
+      password: env('DATABASE_PASSWORD', 'FtYpEmVCa3DbQZy5'),
+      ssl: env.bool('DATABASE_SSL', false),
     },
-  });
+    useNullAsDefault: true
+  }
+});
